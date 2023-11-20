baf833c06e1a2a736316b6be4e44967f150fd857
TG\aashray.trivedi <aashray@techgrains.com>
2023-11-20 15:44:25 +0530

    Trying to Merge

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -10,3 +10,6 @@ select * from Employees
 select U.Name[User's Name],R.Name[Role Name],U.IsActive from Users U left join UserRoles UR on U.Id=UR.UserId left join Roles R on UR.RoleId = R.Id order by UR.Id
 
 
+New Merge Try
+
+
